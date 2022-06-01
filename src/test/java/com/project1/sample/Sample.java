d107745168c4a230ee98f3d570c4f61e5651fe99
Heeraangel <heeraangel.fernandez@gmail.com>
2022-06-01 14:53:39 +0530

    First commit for google

---
+++ src/test/java/com/project1/sample/Sample.java
@@ -0,0 +1,33 @@
+package com.project1.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void sample()
+	{
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary("C:\\Users\\HP\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+		WebDriver driver = null;
+		String url="https://www.google.com/";
+		String browser="chrome";
+		switch(browser)
+		{
+			case "chrome":
+				driver=new ChromeDriver(options);
+				break;
+			case "edge":
+				driver=new EdgeDriver();
+				break;
+		}
+		driver.get(url);
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("wikipedia",Keys.ENTER);
+		driver.quit();
+	}
+}
